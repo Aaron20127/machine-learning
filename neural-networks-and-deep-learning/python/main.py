770954a9778d95ceb6d4a21b41449cfb78a1ae63
Lee <lee@localhost.localdomain>
2018-04-10 17:19:47 +0800

    使用矩阵向量计算反向传播小批量数据，小批量数据越大，反向传播时间越短

--- neural-networks-and-deep-learning/python/main.py
+++ neural-networks-and-deep-learning/python/main.py
@@ -18,5 +18,11 @@ import network
 # 设置神经网络层数和每层的节点数，这里首层和尾层的节点数只能是784和10
 net = network.Network([784, 30, 10])
 
-# 训练并测试
-net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
+# 每个小批量数据越多，矩阵向量反向传播的执行越快
+# SGD_matrix执行反而更慢的原因是组装矩阵时将列表生成矩阵np.array([...])很耗时
+
+net.SGD(training_data, 1, 50000, 3.0, test_data = None)
+net.SGD_matrix(training_data, 1, 5000, 3.0, test_data = None)
+
+net.SGD(training_data, 1, 10000, 3.0, test_data = None)
+net.SGD_matrix(training_data, 1, 10000, 3.0, test_data = None)
