b0d98ddaf94406878125974f1b77bae952e4e89b
Lee <lee@localhost.localdomain>
2018-07-08 10:30:52 +0800

    1.增加画图散点  2.图形中的放大缩小恢复

--- neural-networks-and-deep-learning/common/study_note.py
+++ neural-networks-and-deep-learning/common/study_note.py
@@ -141,7 +141,8 @@ class plotTest:
 
     def plot_base(self, y_coordinate, x_coordinate = [], line_lable = [], 
                 line_color = [], title = '', x_lable = '', y_lable = '',
-                x_limit = [], y_limit = [], y_scale = 'linear'):
+                x_limit = [], y_limit = [], y_scale = 'linear', p_type = [],
+                grad = False):
         """
         描述：画一幅坐标曲线图，可以同时有多条曲线
         参数：y_coordinate （y坐标值，二元列表，例如[[1,2,3],[4,5,6]]，表示有两条曲线，每条曲线的y坐标为[1,2,3]和[4,5,6]）
@@ -152,19 +153,23 @@ class plotTest:
                 title        （整个图片的名称）
                 x_lable      （x轴的含义）
                 y_lable       (y轴的含义)
+                x_limit       (x坐标的显示范围)
+                y_scale       (y轴的单位比例，'linear'常规，'log'对数)
+                p_type        (类型：line线条，scatter散点)
+                grad          (网格)
         """
 
         if (len(x_coordinate) > 0) and (len(y_coordinate) != len(x_coordinate)):
             print "error：x坐标和y坐标不匹配！"
-            return
+            sys.exit()
         
         if (len(line_lable) > 0) and  (len(y_coordinate) != len(line_lable)):
             print "error：线条数和线条名称数不匹配，线条数%d，线条名称数%d！" % \
                     (len(y_coordinate),len(line_lable))     
-            return
+            sys.exit()
 
         if 0 == len(line_color):
-            line_color = ['#9932CC', '#FFA933', '#FF4040', '#CDCD00',
+            line_color = ['#9932CC', '#FF4040' , '#FFA933', '#CDCD00',
                             '#CD8500', '#C0FF3E', '#B8860B', '#AB82FF']
             # print "info: 未指定色彩，使用默认颜色！"
 
@@ -172,34 +177,45 @@ class plotTest:
             print "warning: 指定颜色种类少于线条数，线条%d种，颜色%d种！" % \
                     (len(y_coordinate),len(line_color))
 
-        plt.figure(figsize=(70, 35)) 
-        # ax = plt.subplot(221)
+        # plt.figure(figsize=(70, 35)) 
+        plt.figure() 
         ax = plt.subplot(111)
 
         # 如果没有给x的坐标，设置从0开始计数的整数坐标
-        if 0 == len(x_coordinate):
+        if not x_coordinate:
             x_coordinate = [range(len(y)) for y in y_coordinate]
 
         # 如果没有给线条名称，则使用默认线条名称
-        if 0 == len(line_lable):
+        if not line_lable:
             line_lable = ["line " + str(i) for i in range(len(y_coordinate))]
 
-        for i in range(len(y_coordinate)):
-            ax.plot(x_coordinate[i], y_coordinate[i], color = line_color[i%len(line_color)], \
-                    linewidth = 2.0, label = line_lable[i])       
+        # 如果没有指定图形类型，默认画线条line
+        if not p_type:
+            p_type = ["line" for y in y_coordinate]
 
-        ax.set_title(title, fontsize=14) # 标题
-        ax.set_xlabel(x_lable, fontsize=14) # x坐标的意义
-        ax.set_ylabel(y_lable, fontsize=14) # y坐标的意义
+        for i in range(len(y_coordinate)):
+            if p_type[i] == 'line':
+                ax.plot(x_coordinate[i], y_coordinate[i], color = line_color[i%len(line_color)], \
+                        linewidth = 2.0, label = line_lable[i])      
+            elif p_type[i] == 'scatter': 
+                ax.scatter(x_coordinate[i], y_coordinate[i],  s = 90, c=line_color[i%len(line_color)],\
+                            linewidth = 2.0, alpha=0.6, marker='+', label = line_lable[i])
+            else:
+                print "error：Invalid p_type %s！" % (p_type[i])
+                sys.exit()
+
+        ax.set_title(title) # 标题
+        ax.set_xlabel(x_lable) # x坐标的意义
+        ax.set_ylabel(y_lable) # y坐标的意义
         ax.set_yscale(y_scale) # 'linear','log'
         ### 自适应轴的范围效果更好
         if x_limit: ax.set_xlim(x_limit) # x坐标显示的范围
         if y_limit: ax.set_ylim(y_limit) # y坐标显示范围
         
-        plt.xticks(fontsize=14)
-        plt.yticks(fontsize=14)
-        plt.legend(loc="best", fontsize=14) # 线条的名称显示在右下角
-        plt.grid(True) # 网格
+        # plt.xticks()
+        # plt.yticks()
+        plt.legend(loc="best") # 线条的名称显示在右下角
+        if grad: plt.grid(True) # 网格
 
         # plt.savefig("file.png", dpi = 200)  #保存图片，默认png     
         # plt.show()
@@ -538,7 +554,8 @@ if __name__=="__main__":
     # cPickleTest().test_3()
     # threadTest().test()
     # staticVariableTest().test()
-    matrixTest().test_6()
+    # matrixTest().test_2()
+    plotTest().test()
 
 
 
